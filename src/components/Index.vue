554bdc8f0a55ce7fb5d5162fecc85f41b97e8dc7
wangyuan <wangyuan@xiekaiqiangdeMacBook-Air.local>
2016-05-23 20:12:26 +0800

    test on mac

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,6 +5,7 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title has-text-centered">session status: {{ session }}</h1>
+        <p>Github address: https://github.com/Yuyz0112/MyKita-Vue-Front-End</p>
       </div>
     </div>
     <div class="hero-foot">
